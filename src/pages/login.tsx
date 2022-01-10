6a22c498ce68ff5c8a21991a90f8eb60f186bd06
felix t <41817193+o2bomb@users.noreply.github.com>
2022-01-10 15:08:23 +0800

    Working email login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,34 @@
 import { Alert, Box, Button, Typography } from "@mui/material"
 import { useState } from "react"
 import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login"
+import { useForm } from "react-hook-form"
 import { Link, Redirect } from "react-router-dom"
 import { ReactComponent as FacebookIcon } from "../assets/images/icons/facebook.svg"
 import { ReactComponent as GoogleIcon } from "../assets/images/icons/google.svg"
 import XSYNLogoImage from "../assets/images/XSYN Stack White.svg"
 import { FacebookLogin, ReactFacebookFailureResponse, ReactFacebookLoginInfo } from "../components/facebookLogin"
+import { InputField } from "../components/form/inputField"
 import { LoginMetaMask } from "../components/loginMetaMask"
 import { AuthContainer, useAuth } from "../containers/auth"
+import { useWebsocket } from "../containers/socket"
+import HubKey from "../keys"
+import { PasswordLoginResponse } from "../types/auth"
 
+interface LogInInput {
+    email: string
+    password: string
+}
 
 export const LoginPage: React.FC = () => {
-    const { user } = useAuth()
+    const { send } = useWebsocket()
+    const { user, setUser } = useAuth()
+
     const { loginGoogle, loginFacebook } = AuthContainer.useContainer()
+
+    const { control, handleSubmit, watch, trigger, setError } = useForm<LogInInput>()
     const [errorMessage, setErrorMessage] = useState<string | null>(null)
+    const [loading, setLoading] = useState(false)
+    const [showEmailLogin, setShowEmailLogin] = useState(false)
 
     const onMetaMaskLoginFailure = (error: string) => {
         setErrorMessage(error)
@@ -80,7 +95,72 @@ export const LoginPage: React.FC = () => {
                     marginBottom: "1rem",
                     fontSize: "2rem"
                 }}>Welcome Back</Typography>
-                <Box
+                {showEmailLogin ? <Box component="form" onSubmit={handleSubmit(async (input) => {
+                    try {
+                        setLoading(true)
+                        setErrorMessage(null)
+
+                        const resp = await send<PasswordLoginResponse>(HubKey.AuthLogin, input)
+                        setUser(resp.user)
+                        localStorage.setItem("token", resp.token)
+                    } catch (e) {
+                        setErrorMessage(e === "string" ? e : "Something went wrong, please try again.")
+                    } finally {
+                        setLoading(false)
+                    }
+                })} sx={{
+                    "& > *:not(:last-child)": {
+                        marginBottom: "1rem"
+                    }
+                }}>
+                    <InputField
+                        name="email"
+                        label="Email"
+                        type="email"
+                        control={control}
+                        rules={{
+                            required: "Email is required",
+                            pattern: {
+                                value: /.+@.+\..+/,
+                                message: "Invalid email address",
+                            },
+                        }}
+                        fullWidth
+                        autoFocus
+                        variant="standard"
+                        disabled={loading}
+                    />
+                    <InputField
+                        name="password"
+                        label="Password"
+                        type="password"
+                        control={control}
+                        placeholder="Password"
+                        fullWidth
+                        variant="standard"
+                        disabled={loading}
+                        rules={{
+                            required: "Password is required",
+                        }}
+                    />
+                    <Box sx={{
+                        display: "flex",
+                        "& > *:not(:last-child)": {
+                            marginRight: ".5rem"
+                        }
+                    }}>
+                        <Button type="button" variant="contained" onClick={() => setShowEmailLogin(false)} sx={(theme) => ({
+                            marginLeft: "auto",
+                            backgroundColor: theme.palette.neutral.main
+                        })}>
+                            Back
+                        </Button>
+                        <Button type="submit" variant="contained" color="primary" disabled={loading}>
+                            Log In
+                        </Button>
+                    </Box>
+                    {!!errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+                </Box> : <Box
                     sx={{
                         display: "flex",
                         flexDirection: "column",
@@ -136,7 +216,7 @@ export const LoginPage: React.FC = () => {
                             backgroundColor: theme.palette.text.primary,
                         })} />
                     </Box>
-                    <Button variant="contained" sx={(theme) => ({
+                    <Button onClick={() => setShowEmailLogin(true)} variant="contained" sx={(theme) => ({
                         backgroundColor: theme.palette.neutral.main
                     })}>
                         Email Login
@@ -146,7 +226,7 @@ export const LoginPage: React.FC = () => {
                             {errorMessage}
                         </Alert>
                     )}
-                </Box>
+                </Box>}
             </Box>
         </Box>
     )
