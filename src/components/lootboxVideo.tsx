06adb832a740c7449ea9fff734b125c031ffb2cc
hjpunzalan <hj.punzalan@hotmail.com>
2022-02-27 13:56:41 +0800

    improve ux animation and error handling

--- src/components/lootboxVideo.tsx
+++ src/components/lootboxVideo.tsx
@@ -1,22 +1,30 @@
-import { Box, styled, Button, Fade } from "@mui/material"
-import React from "react"
-import { StringDecoder } from "string_decoder"
+import { Box, Button, Fade, styled } from "@mui/material"
+import React, { useEffect } from "react"
 
 interface VideoProps {
 	srcURL: string
 	setOpen: React.Dispatch<React.SetStateAction<boolean>>
 	open: boolean
+	setDialogOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const LootboxVideo: React.FC<VideoProps> = ({ srcURL, setOpen, open }) => {
+const LootboxVideo: React.FC<VideoProps> = ({ srcURL, setOpen, open, setDialogOpen }) => {
 	const videoURL = {
 		"98bf7bb3-1a7c-4f21-8843-458d62884060": "",
 		"7c6dde21-b067-46cf-9e56-155c88a520e2": "",
 		"880db344-e405-428d-84e5-6ebebab1fe6d": "",
 	}
 
+	useEffect(() => {
+		if (open)
+			setTimeout(() => {
+				setDialogOpen(true)
+				setOpen(false)
+			}, 10000)
+	}, [open, setDialogOpen, setOpen])
+
 	return (
-		<Fade in={open}>
+		<Fade in={open} appear={false} timeout={1000}>
 			<Box sx={{ position: "absolute", height: "100vh", width: "100vw", zIndex: 99 }}>
 				<Video
 					disablePictureInPicture
@@ -27,6 +35,7 @@ const LootboxVideo: React.FC<VideoProps> = ({ srcURL, setOpen, open }) => {
 					muted
 					autoPlay
 					onEnded={() => {
+						setDialogOpen(true)
 						setOpen(false)
 					}}
 				>
@@ -50,12 +59,9 @@ const LootboxVideo: React.FC<VideoProps> = ({ srcURL, setOpen, open }) => {
 export default LootboxVideo
 
 const Video = styled("video")({
-	" video::-internal-media-controls-overlay-cast-button": {
+	" &::-internal-media-controls-overlay-cast-button": {
 		display: "none",
 	},
 	height: "100%",
 	width: "100%",
 })
-function useEffect() {
-	throw new Error("Function not implemented.")
-}
