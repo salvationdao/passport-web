ac5e34e07e5df3ddb68f94d7147ead6a0da7e32e
Ivan Ng <87626385+ivanngyb@users.noreply.github.com>
2022-03-12 16:31:30 +0800

    Modal Error Signing (#166)
    * Added error modal for signing
    
    * Added error modal for signing
    
    * Added error modal for signing
    
    * Added error modal for signing

---
+++ src/components/earlyContributorErrorModal.tsx
@@ -0,0 +1,38 @@
+import { Dialog, DialogActions, DialogContent, DialogTitle, Typography } from "@mui/material"
+
+import { FancyButton } from "./fancyButton"
+
+interface EarlyContributorModalProps {
+	open: boolean
+}
+
+export const EarlyContributorErrorModal = ({ open }: EarlyContributorModalProps) => {
+	return (
+		<Dialog
+			open={open}
+			onClose={() => {
+				window.location.reload()
+			}}
+			maxWidth={"xl"}
+		>
+			<DialogTitle
+				sx={(theme) => ({
+					fontSize: theme.typography.h3,
+				})}
+				color={"primary"}
+			>
+				Failed to sign message. Please try again!
+			</DialogTitle>
+
+			<DialogActions sx={{ display: "flex", width: "100%", justifyContent: "space-between", flexDirection: "row-reverse" }}>
+				<FancyButton
+					onClick={() => {
+						window.location.reload()
+					}}
+				>
+					Try Again
+				</FancyButton>
+			</DialogActions>
+		</Dialog>
+	)
+}
