111efe054b62ed0bd3c106eede0841f8d10ee32d
Ivan Ng <87626385+ivanngyb@users.noreply.github.com>
2022-03-12 13:15:23 +0800

    brought back nav button (#164)

--- src/components/home/navbar.tsx
+++ src/components/home/navbar.tsx
@@ -166,83 +166,80 @@ interface MenuButtonProps {
 
 const MenuButton: React.FC<MenuButtonProps> = ({ sx }) => {
 	const { setSidebarOpen } = useSidebarState()
-	const mobileScreen = useMediaQuery("(max-width:1024px)")
 
-	return mobileScreen ? (
-		<>
-			<LoadingButton
-				onClick={() => setSidebarOpen((prev) => !prev)}
-				sx={{
-					position: "relative",
-					height: "3.3rem",
-					width: "3.3rem",
-					minWidth: "auto",
-					padding: 0,
-					borderRadius: "50%",
-					cursor: "pointer",
-					backgroundColor: "transparent",
-					border: "none",
-					"&:hover": {
-						"& > *:nth-of-type(1)": {
-							transform: "rotate(30deg) translate(-3px, 0)",
-						},
-						"& > *:nth-of-type(2)": {},
-						"& > *:nth-of-type(3)": {
-							transform: "rotate(30deg) translate(3px, 0)",
-						},
+	return (
+		<LoadingButton
+			onClick={() => setSidebarOpen((prev) => !prev)}
+			sx={{
+				position: "relative",
+				height: "3.3rem",
+				width: "3.3rem",
+				minWidth: "auto",
+				padding: 0,
+				borderRadius: "50%",
+				cursor: "pointer",
+				backgroundColor: "transparent",
+				border: "none",
+				"&:hover": {
+					"& > *:nth-of-type(1)": {
+						transform: "rotate(30deg) translate(-3px, 0)",
 					},
-					"&:active": {
-						"& > *:nth-of-type(1)": {
-							transform: "rotate(30deg) translate(1px, 0)",
-						},
-						"& > *:nth-of-type(2)": {},
-						"& > *:nth-of-type(3)": {
-							transform: "rotate(30deg) translate(-1px, 0)",
-						},
+					"& > *:nth-of-type(2)": {},
+					"& > *:nth-of-type(3)": {
+						transform: "rotate(30deg) translate(3px, 0)",
 					},
-					...sx,
-				}}
-			>
-				<Box
-					component="span"
-					sx={(theme) => ({
-						position: "absolute",
-						top: ".5rem",
-						left: ".5rem",
-						height: "2rem",
-						width: ".2rem",
-						transform: "rotate(30deg)",
-						transition: "transform .2s cubic-bezier(0.175, 0.885, 0.32, 1.275)",
-						backgroundColor: theme.palette.common.white,
-					})}
-				/>
-				<Box
-					component="span"
-					sx={(theme) => ({
-						position: "absolute",
-						top: ".5rem",
-						left: "1.5rem",
-						height: "2rem",
-						width: ".2rem",
-						transform: "rotate(30deg)",
-						transition: "transform .2s cubic-bezier(0.175, 0.885, 0.32, 1.275)",
-						backgroundColor: theme.palette.common.white,
-					})}
-				/>
-				<Box
-					component="span"
-					sx={(theme) => ({
-						position: "absolute",
-						top: ".5rem",
-						left: "2.5rem",
-						height: "2rem",
-						width: ".2rem",
-						transform: "rotate(30deg)",
-						transition: "transform .2s cubic-bezier(0.175, 0.885, 0.32, 1.275)",
-						backgroundColor: theme.palette.common.white,
-					})}
-				/>
-			</LoadingButton>
-		</>
-	) : null
+				},
+				"&:active": {
+					"& > *:nth-of-type(1)": {
+						transform: "rotate(30deg) translate(1px, 0)",
+					},
+					"& > *:nth-of-type(2)": {},
+					"& > *:nth-of-type(3)": {
+						transform: "rotate(30deg) translate(-1px, 0)",
+					},
+				},
+				...sx,
+			}}
+		>
+			<Box
+				component="span"
+				sx={(theme) => ({
+					position: "absolute",
+					top: ".5rem",
+					left: ".5rem",
+					height: "2rem",
+					width: ".2rem",
+					transform: "rotate(30deg)",
+					transition: "transform .2s cubic-bezier(0.175, 0.885, 0.32, 1.275)",
+					backgroundColor: theme.palette.common.white,
+				})}
+			/>
+			<Box
+				component="span"
+				sx={(theme) => ({
+					position: "absolute",
+					top: ".5rem",
+					left: "1.5rem",
+					height: "2rem",
+					width: ".2rem",
+					transform: "rotate(30deg)",
+					transition: "transform .2s cubic-bezier(0.175, 0.885, 0.32, 1.275)",
+					backgroundColor: theme.palette.common.white,
+				})}
+			/>
+			<Box
+				component="span"
+				sx={(theme) => ({
+					position: "absolute",
+					top: ".5rem",
+					left: "2.5rem",
+					height: "2rem",
+					width: ".2rem",
+					transform: "rotate(30deg)",
+					transition: "transform .2s cubic-bezier(0.175, 0.885, 0.32, 1.275)",
+					backgroundColor: theme.palette.common.white,
+				})}
+			/>
+		</LoadingButton>
+	)
 }
