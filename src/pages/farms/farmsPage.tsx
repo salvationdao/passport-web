407e2f7338acb62e7c85808d20b606f3cc83b04f
hjpunzalan <hj.punzalan@hotmail.com>
2022-05-06 17:23:47 +0800

    fix APR value, add flag to show APR or not

--- src/pages/farms/farmsPage.tsx
+++ src/pages/farms/farmsPage.tsx
@@ -140,17 +140,21 @@ const LabelContainer = styled("div")({
 })
 
 const FarmInfo = (props: FarmInfoProps) => {
-	// let apr = props.loading ? "--- %" : `${(props.yieldPercentage !== Infinity ? props.yieldPercentage * 100 : 0).toFixed(4)}%`
+	let apr = props.loading ? "--- %" : `${(props.yieldPercentage !== Infinity ? props.yieldPercentage * 100 : 0).toFixed(4)}%`
 
-	// if (props.yieldPercentage === 0) {
-	// 	apr = "--- %"
-	// }
+	if (props.yieldPercentage === 0) {
+		apr = "--- %"
+	}
+
+	const showAPR = false
 	return (
 		<Stack gap=".2rem" justifyContent="center" sx={{ width: "100%" }}>
 			<Stack justifyContent="space-between" sx={{ width: "100%" }}>
-				{/* <LabelContainer>
-					<InfoLabel>APR:</InfoLabel> <InfoValue>{apr}</InfoValue>
-				</LabelContainer> */}
+				{showAPR && (
+					<LabelContainer>
+						<InfoLabel>APR:</InfoLabel> <InfoValue>{apr}</InfoValue>
+					</LabelContainer>
+				)}
 			</Stack>
 			<LabelContainer>
 				<InfoLabel>Next phase in:</InfoLabel>
