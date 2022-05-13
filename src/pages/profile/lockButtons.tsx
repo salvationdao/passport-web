8ec204af6ad14807b17bacafad08c7650ab3543f
alyssa <aruefenacht14@gmail.com>
2022-05-13 13:51:16 +0800

    modal styles

--- src/pages/profile/lockButtons.tsx
+++ src/pages/profile/lockButtons.tsx
@@ -111,12 +111,17 @@ export const LockModal = ({ open, option, setOpen }: LockBaseProps & LockModalPr
 					sx={{
 						px: "3.2rem",
 						py: "2.4rem",
+						display: "flex",
+						flexDirection: "column",
 					}}
 				>
 					<Stack spacing={2}>
-						<Box sx={{ display: "flex", alignItems: "center" }}>
-							<WarningAmberIcon color="warning" sx={{ fontSize: "3rem", mr: "1.3rem" }} />
-							<Typography variant="h5">{`Locking ${option?.type}`}</Typography>
+						<Box sx={{ display: "flex" }}>
+							<WarningAmberIcon color="warning" sx={{ fontSize: "3rem", mr: "1rem" }} />
+							<Typography
+								variant="h5"
+								sx={{ fontSize: "2rem", textTransform: "uppercase", alignSelf: "flex-end" }}
+							>{`Locking ${option?.type}`}</Typography>
 						</Box>
 						<Divider />
 
@@ -128,19 +133,22 @@ export const LockModal = ({ open, option, setOpen }: LockBaseProps & LockModalPr
 						<Typography variant="body2">In order to get your unlock your account, you will have to speak to an Admin directly.</Typography>
 					</Stack>
 
-					<StyledFancyButton variant="outlined" onClick={() => setOpen(false)}>
-						Cancel
-					</StyledFancyButton>
-					<StyledFancyButton
-						variant="outlined"
-						onClick={() => {
-							if (!option) return
-							lockRequest(option.type)
-							setOpen(false)
-						}}
-					>
-						Confirm
-					</StyledFancyButton>
+					<Stack direction="row" spacing="1rem" sx={{ alignSelf: "flex-end", mt: "1rem" }}>
+						<StyledFancyButton variant="outlined" onClick={() => setOpen(false)}>
+							Cancel
+						</StyledFancyButton>
+						<StyledFancyButton
+							variant="outlined"
+							sx={{ backgroundColor: colors.neonPink }}
+							onClick={() => {
+								if (!option) return
+								lockRequest(option.type)
+								setOpen(false)
+							}}
+						>
+							Confirm
+						</StyledFancyButton>
+					</Stack>
 				</Box>
 			</Box>
 		</Modal>
