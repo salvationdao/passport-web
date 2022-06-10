f26346e96a1663b186245e466556adb1dee2fc30
Owen Giri <owengiri20@gmail.com>
2022-06-10 09:34:23 +0800

    Remove email login (#248)
    * remove social/email logins
    
    * remove signup link
    
    * lint warns
    
    * lint warns
    
    * lint warns

--- src/pages/login/form.tsx
+++ src/pages/login/form.tsx
@@ -6,10 +6,8 @@ import { Alert, Box } from "@mui/material"
 import { MetaMaskLogin } from "../../components/loginMetaMask"
 import { FancyButton } from "../../components/fancyButton"
 import { MetaMaskIcon, WalletConnectIcon } from "../../assets"
-import EmailLogin from "./email"
 import { SyntheticEvent, useState } from "react"
 import { useAuth } from "../../containers/auth"
-import Link from "@mui/material/Link"
 
 const LoginForm = () => {
 	const { authType, setAuthType } = useAuth()
@@ -22,8 +20,6 @@ const LoginForm = () => {
 		<Box component={TabContext} sx={{ display: "flex", flexDirection: "column", flex: 1, width: "100%" }} value={authType}>
 			<TabList onChange={handleChange} aria-label="Pick login type" variant="fullWidth">
 				<Tab label="Wallet" value="wallet" />
-				<Tab label="Email" value="email" />
-				<Tab label="Socials" value="socials" />
 			</TabList>
 			<Box sx={{ borderTop: 1, borderColor: "divider", padding: "20px", flex: 1, display: "flex" }}>
 				{authType === "wallet" && (
@@ -55,13 +51,6 @@ const LoginForm = () => {
 					</Box>
 				)}
 
-				{authType === "email" && <EmailLogin />}
-				{authType === "socials" && <Box>Socials</Box>}
-			</Box>
-			<Box sx={{ p: 3 }}>
-				<Link href="#" variant="body2">
-					{"Don't have an account? Sign Up"}
-				</Link>
 			</Box>
 			{error && <Alert severity="error">{error}</Alert>}
 		</Box>
