aa23ff9d6b542b3d8f6c423db7c0d0ce7a58f702
vincentwillats <vincentwillats.software@gmail.com>
2022-11-04 10:00:51 +0800

    commented out bunch of code while withdraws are disabled

--- src/pages/withdraw/withdrawPage.tsx
+++ src/pages/withdraw/withdrawPage.tsx
@@ -1,48 +1,48 @@
-import { Box, Paper } from "@mui/material"
-import { BigNumber } from "ethers"
-import { formatUnits } from "ethers/lib/utils"
-import React, { useEffect, useState } from "react"
+import { Box, Paper, Typography } from "@mui/material"
+// import { BigNumber } from "ethers"
+// import { formatUnits } from "ethers/lib/utils"
+import React from "react"
 import { Navbar } from "../../components/home/navbar"
-import { TransactionResultOverlay } from "../../components/transferStatesOverlay/transactionResultOverlay"
-import { WithdrawSups } from "../../components/withdrawSups"
-import { API_ENDPOINT_HOSTNAME } from "../../config"
-import { useWeb3 } from "../../containers/web3"
-import { AddressDisplay } from "../../helpers/web3"
-import { transferStateType } from "../../types/types"
-import { usePassportCommandsUser } from "../../hooks/usePassport"
-import { useAuth } from "../../containers/auth"
+// import { TransactionResultOverlay } from "../../components/transferStatesOverlay/transactionResultOverlay"
+// import { WithdrawSups } from "../../components/withdrawSups"
+// import { API_ENDPOINT_HOSTNAME } from "../../config"
+// import { useWeb3 } from "../../containers/web3"
+// import { AddressDisplay } from "../../helpers/web3"
+// import { transferStateType } from "../../types/types"
+// import { usePassportCommandsUser } from "../../hooks/usePassport"
+// import { useAuth } from "../../containers/auth"
 
-interface CanEnterResponse {
-	can_withdraw: boolean
-}
+// interface CanEnterResponse {
+// 	can_withdraw: boolean
+// }
 
 export const WithdrawPage = () => {
-	const { account } = useWeb3()
-	const { user } = useAuth()
-	const { state, send } = usePassportCommandsUser("/commander")
+	// const { account } = useWeb3()
+	// const { user } = useAuth()
+	// const { state, send } = usePassportCommandsUser("/commander")
+	//
+	// const [currentTransferHash, setCurrentTransferHash] = useState<string>("")
+	//
+	// //TODO: set this transferstate to "none" when withdrawSUPs functionality becomes available
+	// const [currentTransferState, setCurrentTransferState] = useState<transferStateType>("unavailable")
+	// const [loading, setLoading] = useState<boolean>(false)
+	// const [error, setError] = useState<string>("")
+	// const [withdrawAmount, setWithdrawAmount] = useState<BigNumber>(BigNumber.from(0))
 
-	const [currentTransferHash, setCurrentTransferHash] = useState<string>("")
-
-	//TODO: set this transferstate to "none" when withdrawSUPs functionality becomes available
-	const [currentTransferState, setCurrentTransferState] = useState<transferStateType>("unavailable")
-	const [loading, setLoading] = useState<boolean>(false)
-	const [error, setError] = useState<string>("")
-	const [withdrawAmount, setWithdrawAmount] = useState<BigNumber>(BigNumber.from(0))
-
-	useEffect(() => {
-		try {
-			;(async () => {
-				const resp = await fetch(`${window.location.protocol}//${API_ENDPOINT_HOSTNAME}/api/withdraw/check`)
-				const body = (await resp.clone().json()) as CanEnterResponse
-				if (body.can_withdraw) {
-					setCurrentTransferState("none")
-					return
-				}
-			})()
-		} catch (e) {
-			console.error(e)
-		}
-	}, [])
+	// useEffect(() => {
+	// 	try {
+	// 		;(async () => {
+	// 			const resp = await fetch(`${window.location.protocol}//${API_ENDPOINT_HOSTNAME}/api/withdraw/check`)
+	// 			const body = (await resp.clone().json()) as CanEnterResponse
+	// 			if (body.can_withdraw) {
+	// 				setCurrentTransferState("none")
+	// 				return
+	// 			}
+	// 		})()
+	// 	} catch (e) {
+	// 		console.error(e)
+	// 	}
+	// }, [])
 
 	return (
 		<Box
@@ -74,16 +74,16 @@ export const WithdrawPage = () => {
 						position: "relative",
 					}}
 				>
-					<TransactionResultOverlay
-						currentTransferState={currentTransferState}
-						setCurrentTransferState={setCurrentTransferState}
-						currentTransferHash={currentTransferHash}
-						confirmationMessage={`Withdrawing ${withdrawAmount ? formatUnits(withdrawAmount) : "NONE"} $SUPS from users: ${
-							user?.username
-						} to wallet address: ${account ? AddressDisplay(account) : null}.`}
-						error={error}
-						loading={loading}
-					/>
+					{/*<TransactionResultOverlay*/}
+					{/*	currentTransferState={currentTransferState}*/}
+					{/*	setCurrentTransferState={setCurrentTransferState}*/}
+					{/*	currentTransferHash={currentTransferHash}*/}
+					{/*	confirmationMessage={`Withdrawing ${withdrawAmount ? formatUnits(withdrawAmount) : "NONE"} $SUPS from users: ${*/}
+					{/*		user?.username*/}
+					{/*	} to wallet address: ${account ? AddressDisplay(account) : null}.`}*/}
+					{/*	error={error}*/}
+					{/*	loading={loading}*/}
+					{/*/>*/}
 					<Box
 						sx={{
 							width: "80%",
@@ -93,18 +93,22 @@ export const WithdrawPage = () => {
 							alignItems: "center",
 						}}
 					>
-						<WithdrawSups
-							setCurrentTransferState={setCurrentTransferState}
-							currentTransferState={currentTransferState}
-							withdrawAmount={withdrawAmount}
-							setWithdrawAmount={setWithdrawAmount}
-							setError={setError}
-							setCurrentTransferHash={setCurrentTransferHash}
-							setLoading={setLoading}
-							user={user}
-							send={send}
-							state={state}
-						/>
+						<Typography variant={"h4"}>
+							Withdrawal of SUPS are currently paused while we transition over to the Ethereum network. Sorry for any inconvenience
+							caused.
+						</Typography>
+						{/*<WithdrawSups*/}
+						{/*	setCurrentTransferState={setCurrentTransferState}*/}
+						{/*	currentTransferState={currentTransferState}*/}
+						{/*	withdrawAmount={withdrawAmount}*/}
+						{/*	setWithdrawAmount={setWithdrawAmount}*/}
+						{/*	setError={setError}*/}
+						{/*	setCurrentTransferHash={setCurrentTransferHash}*/}
+						{/*	setLoading={setLoading}*/}
+						{/*	user={user}*/}
+						{/*	send={send}*/}
+						{/*	state={state}*/}
+						{/*/>*/}
 					</Box>
 				</Paper>
 			</Box>
